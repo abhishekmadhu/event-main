0fc29d9155decf35ea9df5a43b25308c6a1c94c1
Abhishek Madhu <abhishek.prince.madhu@gmail.com>
2019-01-05 17:55:14 +0530

    photo gallery partially working. Fallback here now

--- assets/js/other.js
+++ assets/js/other.js
@@ -1,24 +1,4 @@
-/*	header bg changer	*/
-function displayNextBgImage() {
-    x = (x === images.length - 1) ? 0 : x + 1;
-    document.getElementByClassName("scroll-image-gallery").src = imagesTwo[x];
-}
-
-function displayPreviousBgImage() {
-    x = (x <= 0) ? images.length - 1 : x - 1;
-    document.getElementByClassName("scroll-image-gallery").src = imagesTwo[x];
-}
-
-function startBgTimer() {
-    setInterval(displayNextImage, 5000);
-}
-
-var imagesTwo = [], x = -1;
-imagesTwo[0] = "image1.jpg";
-imagesTwo[1] = "image2.jpg";
-imagesTwo[2] = "image3.jpg";
-
-/*   Actual Image changer   */
+/*   Header Image changer   */
 
 function displayNextImage() {
     x = (x === images.length - 1) ? 0 : x + 1;
@@ -38,3 +18,29 @@ var images = [], x = -1;
 images[0] = "url(image1.jpg)";
 images[1] = "url(image2.jpg)";
 images[2] = "url(image3.jpg)";
+
+/****************************************************************************/
+
+function displayNextPhoto(){
+	y = (y === photos.length - 1) ? 0 : y + 1;
+	document.getElementById("image-to-be-changed").src = photos[x];
+}
+
+function displayPreviousPhoto() {
+	// body...
+	y = (y <= 0) ? photos.length - 1 : y - 1;
+	document.getElementById("image-to-be-changed").src = photos[x];
+}
+
+var photos = [], y = -1;
+photos[0] = "image1.jpg";
+photos[1] = "image2.jpg";
+photos[2] = "image3.jpg";
+
+$(document).ready(function() {
+    $("#right-arrow-two").click(function() {
+        alert("button is clicked");
+        displayNextPhoto();
+    });
+});
+
