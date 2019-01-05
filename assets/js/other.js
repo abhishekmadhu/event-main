fa9c07fba20064426f480565ee4e58e2fda95fea
Abhishek Madhu <abhishek.prince.madhu@gmail.com>
2019-01-05 18:17:33 +0530

    scary commit

--- assets/js/other.js
+++ assets/js/other.js
@@ -1,26 +1,3 @@
-/*   Header Image changer   */
-
-function displayNextImage() {
-    x = (x === images.length - 1) ? 0 : x + 1;
-    document.getElementById("site-header").style.backgroundImage = images[x];
-}
-
-function displayPreviousImage() {
-    x = (x <= 0) ? images.length - 1 : x - 1;
-    document.getElementById("site-header").style.backgroundImage = images[x];
-}
-
-function startTimer() {
-    setInterval(displayNextImage, 5000);
-}
-
-var images = [], x = -1;
-images[0] = "url(image1.jpg)";
-images[1] = "url(image2.jpg)";
-images[2] = "url(image3.jpg)";
-
-/****************************************************************************/
-
 function displayNextPhoto(){
 	y = (y === photos.length - 1) ? 0 : y + 1;
 	document.getElementById("image-to-be-changed").src = photos[x];
@@ -37,10 +14,37 @@ photos[0] = "image1.jpg";
 photos[1] = "image2.jpg";
 photos[2] = "image3.jpg";
 
-$(document).ready(function() {
-    $("#right-arrow-two").click(function() {
-        alert("button is clicked");
-        displayNextPhoto();
-    });
+$("#right-arrow-two").click(function() {
+    displayNextPhoto();
 });
+
+$("#right-arrow-one").click(function() {
+    displayPreviousPhoto();
+});
+
+/***************************************************************************/
+
+/*   Header Image changer   */
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("site-header").style.backgroundImage = images[x];
+}
+
+function displayPreviousImage() {
+    x = (x <= 0) ? images.length - 1 : x - 1;
+    document.getElementById("site-header").style.backgroundImage = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 7000);
+    setInterval(displayNextPhoto, 3000);
+}
+
+var images = [], x = -1;
+images[0] = "url(image1.jpg)";
+images[1] = "url(image2.jpg)";
+images[2] = "url(image3.jpg)";
+
+/****************************************************************************/
 
