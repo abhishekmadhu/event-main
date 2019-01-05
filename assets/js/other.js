5dc96458b7847aa08146bc8c90b4d7feeae716bf
Abhishek Madhu <abhishek.prince.madhu@gmail.com>
2019-01-06 00:51:09 +0530

    horizontal bar okay

--- assets/js/other.js
+++ assets/js/other.js
@@ -58,3 +58,22 @@ images[2] = "url(image3.jpg)";
 
 /****************************************************************************/
 
+/********************************TEST PART***********************************/
+
+// $(document).ready(function () {
+//      $('a[href^="#"]').on('click', function (e) {
+//          e.preventDefault();
+
+//          var target = this.hash,
+//              $target = $(target);
+
+//          $('html, body').stop().animate({
+//              'scrollTop': $target.offset().top - 80
+//          }, 900, 'swing', function () {
+//              // window.location.hash = target;
+//          });
+//      });
+//  });
+
+
+smoothScroll.init();
